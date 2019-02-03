382c53c02cbb636f756c345132ce53bf528d6d45
pannarit <pannarit09@outlook.com>
2019-02-03 11:27:58 +0700

    test

--- myproject2/myapp/models.py
+++ myproject2/myapp/models.py
@@ -6,3 +6,6 @@ class Item(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits = 5,decimal_places = 2)
     expire = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.id}_{self.name}'
