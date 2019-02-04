5c3f97d1efef3ab4fcabb0526ba79d9cfe1e0bf9
pannarit <pannarit09@outlook.com>
2019-02-04 08:12:33 +0700

    test

--- myproject2/myapp/views.py
+++ myproject2/myapp/views.py
@@ -11,7 +11,83 @@ def current_datetime(request):
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
 
-
 def item_list(request):
-    template  = loader.get_template("index.html")
-    return HttpResponse(template.render())
+    html='''
+
+<title>Fruit Shop</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<style>
+.mySlides {display:none;}
+body {margin:25px;}
+
+div.polaroid {
+  width: 30%;
+  background-color: white;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  margin-bottom: 25px;
+}
+
+div.container {
+  text-align: center;
+  padding: 10px 20px;
+}
+
+</style>
+<body>
+
+<h1 class="w3-center"><font face="Chilanka" color="green">Welcome to My Fruit Shop</font></h1>
+
+<div class="w3-content w3-display-container">
+  <img class="mySlides" src="http://yesofcorsa.com/wp-content/uploads/2015/08/1471_fruit.jpg" style="width:100%">
+  <img class="mySlides" src="http://yesofcorsa.com/wp-content/uploads/2015/08/2553_fruit.jpg" style="width:100%">
+  <img class="mySlides" src="http://yesofcorsa.com/wp-content/uploads/2015/08/4908_fruit.jpg" style="width:100%">
+
+  <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+  <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
+</div>
+
+<script>
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+</script>
+<br>
+
+<div class="w3-container">
+  <div class="w3-card" style="width:25%">
+    <img src="https://cdn-ssl.vidible.tv/prod/2015-04/06/55224e34e4b025fd8795401d_cv1.jpg" alt="Person" style="width:70%">
+    <div class="w3-container">
+      <h4><b>Simon</b></h4>
+      <p>The boss of all bosses</p>
+    </div>  
+  </div>
+  <br>
+
+</body>
+
+'''
+
+
+
+    for i in Item.objects.all():
+        html = html + '<p>%s %s %s %s</p><br>'%(i.name, i.description, i.price, i.expire)
+    return HttpResponse(html)
+     
+   
